4ea1091f42bbeb8782ab854eda3aa5f3632c851d
NikitaManta <54374717+NikitaManta@users.noreply.github.com>
2019-09-26 17:54:26 -0400

    Add Game

---
+++ Plinko/Assets/Scripts/AddForceToBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceToBall : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-transform.right * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * moveSpeed, ForceMode2D.Impulse);
+        }
+
+    }
+}
---
+++ Plinko/Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var objectName = collision.gameObject.name;
+        if (objectName == "Score0-1" || objectName == "Score0-2" || objectName == "Score0-3")
+        {
+            Scorekeeper.scoreValue = 0;
+        }
+
+        if (objectName == "Score50-1" || objectName == "Score50-2")
+        {
+            Scorekeeper.scoreValue = 50;
+        }
+
+        if (objectName == "Score100-1" || objectName == "Score100-2")
+        {
+            Scorekeeper.scoreValue = 100;
+        }
+    }
+}
